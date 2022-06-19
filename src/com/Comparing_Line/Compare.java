2bcc6d008a03ba9b462acbcf0612e9e50f686032
Robins-2603 <robinsforwork@gmail.com>
2022-06-19 23:43:27 +0530

    Added: UC2 for equality check

--- src/com/Comparing_Line/Compare.java
+++ src/com/Comparing_Line/Compare.java
@@ -6,15 +6,41 @@ public class Compare {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison computation program");
         Scanner scan = new Scanner(System.in);
+
+        // For Line 1
         System.out.println("Enter x1 and y1");
-        int x1 = scan.nextInt();
-        int y1 = scan.nextInt();
+        double x1, y1, x2, y2, length1;
+        x1 = scan.nextDouble();
+        y1 = scan.nextDouble();
+
         System.out.println("Enter x2 and y2");
-        int x2 = scan.nextInt();
-        int y2 = scan.nextInt();
-        double length;
-        length = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-        System.out.println("Length of Line1 is " + length );
+        x2 = scan.nextDouble();
+        y2 = scan.nextDouble();
+        length1 = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2) );
+
+        System.out.println("Length of Line1 is " + length1 );
+
+        // For Line 2
+
+        double a1, b1, a2, b2, length2;
+        System.out.println("Enter a1 and b1");
+        a1 = scan.nextDouble();
+        b1 = scan.nextDouble();
+
+        System.out.println("Enter a2 and b2");
+        a2 = scan.nextDouble();
+        b2 = scan.nextDouble();
+        length2 = Math.sqrt(Math.pow((a2-a1),2) + Math.pow((b2-b1),2) );
+
+        System.out.println("Length of Line2 is " + length2 );
+
+        // For Equality check of both line
+        if ( length1 == length2 ) {
+            System.out.println("Both lines are equal");
+        }
+        else {
+            System.out.println("Both lines are not equal");
+        }
 
     }
 }
