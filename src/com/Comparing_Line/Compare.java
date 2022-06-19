82fa0d9f9ef9859218880a1643f482b36e05fbf9
Robins-2603 <robinsforwork@gmail.com>
2022-06-19 20:32:04 +0530

    Added: Welcome message to program

---
+++ src/com/Comparing_Line/Compare.java
@@ -0,0 +1,7 @@
+package com.Comparing_Line;
+
+public class Compare {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison computation program");
+    }
+}
