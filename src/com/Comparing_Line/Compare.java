145701f200b85518c9473e9770490af2856fa832
Robins-2603 <robinsforwork@gmail.com>
2022-06-25 00:54:19 +0530

    Added: Line comparison using OOps Concepts

--- src/com/Comparing_Line/Compare.java
+++ src/com/Comparing_Line/Compare.java
@@ -3,38 +3,28 @@ package com.Comparing_Line;
 import java.util.Scanner;
 
 public class Compare {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison computation program");
-        Scanner scan = new Scanner(System.in);
+    public static double x1, y1, x2, y2, length, length1, length2;
 
-        // For Line 1
+    //method for CoOrdinates of line
+    public static void   coOrdinates(){
         System.out.println("Enter x1 and y1");
-        double x1, y1, x2, y2, length1;
+        Scanner scan = new Scanner(System.in);
         x1 = scan.nextDouble();
         y1 = scan.nextDouble();
-
         System.out.println("Enter x2 and y2");
         x2 = scan.nextDouble();
         y2 = scan.nextDouble();
-        length1 = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2) );
+    }
 
-        System.out.println("Length of Line1 is " + length1 );
+    //method for finding length
 
-        // For Line 2
+    public static double lengthOfLine(){
+        length = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2) );
+        return length;
+    }
 
-        double a1, b1, a2, b2, length2;
-        System.out.println("Enter a1 and b1");
-        a1 = scan.nextDouble();
-        b1 = scan.nextDouble();
-
-        System.out.println("Enter a2 and b2");
-        a2 = scan.nextDouble();
-        b2 = scan.nextDouble();
-        length2 = Math.sqrt(Math.pow((a2-a1),2) + Math.pow((b2-b1),2) );
-
-        System.out.println("Length of Line2 is " + length2 );
-
-        // For Equality check of both line
+    // Method for Comparing
+    public static void  CompareLine(){
         if ( length1 > length2 ) {
             System.out.println("length of Line1 is greater than Line2 by " + (length1 - length2));
         }
@@ -44,6 +34,43 @@ public class Compare {
         else {
             System.out.println("Both Lines are equal");
         }
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison computation program");
+        //for Line 1
+        coOrdinates();
+        // length
+        Compare Line_1 = new Compare();
+        length1 = Line_1.lengthOfLine();
+        System.out.println("length of Line 1 = " + length1);
+
+        // For Line 2
+
+        coOrdinates();
+        // length
+        Compare Line_2 = new Compare();
+        length2 = Line_2.lengthOfLine();
+        System.out.println("length of Line 2 = " + length2);
+
+        //comparing by calling compareLine method
+        CompareLine();
+
+//        double a1, b1, a2, b2, length2;
+//        System.out.println("Enter a1 and b1");
+//        a1 = scan.nextDouble();
+//        b1 = scan.nextDouble();
+//
+//        System.out.println("Enter a2 and b2");
+//        a2 = scan.nextDouble();
+//        b2 = scan.nextDouble();
+//        length2 = Math.sqrt(Math.pow((a2-a1),2) + Math.pow((b2-b1),2) );
+//
+//        System.out.println("Length of Line2 is " + length2 );
+
+        // For Equality check of both line
 
     }
 }
